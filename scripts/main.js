9384dc1231a82c7f24e3d83d6f2a1d7e55964cde
unknown <nhquoc99@gmail.com>
2022-06-06 23:53:53 +0700

    Completed responsive web

--- scripts/main.js
+++ scripts/main.js
@@ -46,7 +46,18 @@ $('.topstory-advertise').slick({
     autoplay: true,
     autoplaySpeed: 3000,
     prevArrow: '<span class="prev-arrow"><i class="fa-solid fa-angle-left"></i></span>',
-    nextArrow: '<span class="next-arrow"><i class="fa-solid fa-angle-right"></i></span>'
+    nextArrow: '<span class="next-arrow"><i class="fa-solid fa-angle-right"></i></span>',
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+            }
+          }
+    ]
+    
   });
 
 
@@ -71,3 +82,29 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp)
+
+
+const menuToggle = document.querySelector('.nav__show-all')
+const menu = document.querySelector('.nav-wrap-menu')
+const closeBtn = document.querySelector('.close-menu')
+const menuToggleTop = document.querySelector('.header-menu-top')
+console.log(menuToggleTop);
+
+if(menuToggleTop) {
+    menuToggleTop.addEventListener('click', () => {
+        menu.classList.toggle('open-menu')
+    })
+}
+
+
+if(menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        menu.classList.add('open-menu')
+    })
+}
+
+if(closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('open-menu')
+    })
+}
